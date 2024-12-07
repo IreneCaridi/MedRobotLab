2fa9bc8481d99170c2cdd6e5291ef58714ccfe85
francescoleni <francesco.leni@mail.polimi.it>
2024-12-07 16:39:48 +0100

    requirements

--- fra_sam_experiments/train_many.py
+++ fra_sam_experiments/train_many.py
@@ -15,29 +15,29 @@ import subprocess
 dataset_path = "/MedRobotLab/dataset" #questo da non modificare
 
 
-batch_size = 4 # questa da modificare a piacere
+batch_size = 4  # questa da modificare a piacere
 
 
 arguments_list = [
-    ["--student", "RepViT", "--reshape_size", "1024", "--epochs", "100", "--batch_size", batch_size,
+    ["--student", "RepViT", "--reshape_size", "1024", "--epochs", "100", "--batch_size", f'{batch_size}',
      "--name", "RepViT_enc_100epcs", "--sched", "cos_lr", "--lr0", "1.25e-2", "--lrf", "5e-5", "--weight_decay", "0.01",
      "--patience", "10", "--device", "gpu", "--AMP", "--as_encoder", "--Kvasir", "--Cholect", "--AtlasDione",
-     "--data_path", dataset_path, '--n_workers', '7'],
+     "--data_path", f'{dataset_path}', '--n_workers', '7'],
 
-    ["--student", "UnetEncoder", "--reshape_size", "512", "--epochs", "100", "--batch_size", batch_size,
+    ["--student", "UnetEncoder", "--reshape_size", "512", "--epochs", "100", "--batch_size", f'{batch_size}',
      "--name", "Unet_enc_100epcs", "--sched", "cos_lr", "--lr0", "1.25e-2", "--lrf", "5e-5", "--weight_decay", "0.01",
      "--patience", "10", "--device", "gpu", "--AMP", "--as_encoder", "--Kvasir", "--Cholect", "--AtlasDione",
-     "--data_path", dataset_path, '--n_workers', '7'],
+     "--data_path", f'{dataset_path}', '--n_workers', '7'],
 
-["--student", "RepViT", "--reshape_size", "1024", "--epochs", "200", "--batch_size", batch_size,
+["--student", "RepViT", "--reshape_size", "1024", "--epochs", "200", "--batch_size", f'{batch_size}',
      "--name", "RepViT_enc_200epcs", "--sched", "cos_lr", "--lr0", "1.25e-2", "--lrf", "5e-5", "--weight_decay", "0.01",
      "--patience", "20", "--device", "gpu", "--AMP", "--as_encoder", "--Kvasir", "--Cholect", "--AtlasDione",
-     "--data_path", dataset_path, '--n_workers', '7'],
+     "--data_path", f'{dataset_path}', '--n_workers', '7'],
 
-    ["--student", "UnetEncoder", "--reshape_size", "512", "--epochs", "200", "--batch_size", batch_size,
+    ["--student", "UnetEncoder", "--reshape_size", "512", "--epochs", "200", "--batch_size", f'{batch_size}',
      "--name", "Unet_enc_200epcs", "--sched", "cos_lr", "--lr0", "1.25e-2", "--lrf", "5e-5", "--weight_decay", "0.01",
      "--patience", "20", "--device", "gpu", "--AMP", "--as_encoder", "--Kvasir", "--Cholect", "--AtlasDione",
-     "--data_path", dataset_path, '--n_workers', '7']
+     "--data_path", f'{dataset_path}', '--n_workers', '7']
 ]
 
 
