29ae574747019e9f51d04bc1bfc9bee36ceca6a1
FrancescoLeni <francesco.leni@mail.polimi.it>
2024-12-17 09:59:08 +0100

    bug fix

--- fra_sam_experiments/models/Rep_ViT.py
+++ fra_sam_experiments/models/Rep_ViT.py
@@ -9,12 +9,12 @@ import torch.nn.functional as F
 from timm.models.vision_transformer import trunc_normal_
 from timm.models.layers import SqueezeExcite
 
-from mmdet.models.dense_heads import YOLOXHead
-from mmdet.models.necks import FPN
-from mmdet.utils import (ConfigType, OptConfigType)
-from mmengine.config import ConfigDict
-from mmdet.models.task_modules.assigners import MaxIoUAssigner
-from mmengine.structures import InstanceData
+# from mmdet.models.dense_heads import YOLOXHead
+# from mmdet.models.necks import FPN
+# from mmdet.utils import (ConfigType, OptConfigType)
+# from mmengine.config import ConfigDict
+# from mmdet.models.task_modules.assigners import MaxIoUAssigner
+# from mmengine.structures import InstanceData
 
 
 __all__ = ['RepViT', 'LayerNorm2d']
@@ -601,61 +601,61 @@ class RepViTUnet(nn.Module):
     def forward(self, x):
         return self.decoder(self.encoder(x))
 
-
-class RpnYolox(nn.Module):
-    def __init__(self, input_shape):
-        super().__init__()
-
-        # upgrade to handle archs
-
-        self.cfg = dict(rpn=dict(assigner=dict(type=MaxIoUAssigner, pos_iou_thr=0.7, neg_iou_thr=0.3, min_pos_iou=0.2,
-                                          match_low_quality=True, ignore_iof_thr=-1)),
-                   nms=ConfigDict(nms_pre=2000, min_bbox_size=0, score_thr=0.35, nms=dict(type='nms', iou_threshold=0.7),
-                                  max_per_img=1000),
-                   metas = dict(img_shape=(input_shape, input_shape))
-        )
-        self.fpn = FPN(in_channels=[96, 192, 384], out_channels=96, num_outs=5)
-        self.head = YOLOXHead(num_classes=2, in_channels=96, strides=[8, 16, 32, 64, 128], train_cfg=self.cfg['rpn'])
-
-    def forward(self, x):
-        x_, _ ,_ = x
-
-        B, C, H, W = x_.shape
-
-        x = self.fpn([*x])
-        x_raw = self.head(x)
-
-        batched_img_metas = [self.cfg['metas']] * B
-        x = self.head.predict_by_feat(*x_raw, batch_img_metas=batched_img_metas, cfg=self.cfg['nms'], with_nms=True)
-
-        return x_raw, x
-
-    def get_loss(self, x_raw, y: [InstanceData]):
-        batched_img_metas = [self.cfg['metas']] * len(y)
-
-        loss_dict = self.head.loss_by_feat(*x_raw, batch_img_metas=batched_img_metas, batch_gt_instances=y)
-
-        return loss_dict
-
-class RepViTDet(nn.Module):
-    def __init__(self, arch='m1', n_classes=1, img_size=1024, fuse=True, freeze=True):
-        super().__init__()
-        # upgrade to handle archs
-
-        self.backbone = RepViT(arch, img_size, fuse, freeze, True,
-                               out_indices=['stage1', 'stage2', 'stage3', 'final'])
-        self.rpn = RpnYolox(input_shape=img_size)
-
-    def forward(self, x):
-
-        x_enc = self.backbone(x)
-
-        x1, x2, x3, xf = x_enc
-
-        x_raw, x_pred = self.rpn([x1, x2, x3])
-
-        return xf, x_raw, x_pred
-
-    def get_loss(self, x_raw, y):
-        return self.rpn.get_loss(x_raw, y)
-
+#
+# class RpnYolox(nn.Module):
+#     def __init__(self, input_shape):
+#         super().__init__()
+#
+#         # upgrade to handle archs
+#
+#         self.cfg = dict(rpn=dict(assigner=dict(type=MaxIoUAssigner, pos_iou_thr=0.7, neg_iou_thr=0.3, min_pos_iou=0.2,
+#                                           match_low_quality=True, ignore_iof_thr=-1)),
+#                    nms=ConfigDict(nms_pre=2000, min_bbox_size=0, score_thr=0.35, nms=dict(type='nms', iou_threshold=0.7),
+#                                   max_per_img=1000),
+#                    metas = dict(img_shape=(input_shape, input_shape))
+#         )
+#         self.fpn = FPN(in_channels=[96, 192, 384], out_channels=96, num_outs=5)
+#         self.head = YOLOXHead(num_classes=2, in_channels=96, strides=[8, 16, 32, 64, 128], train_cfg=self.cfg['rpn'])
+#
+#     def forward(self, x):
+#         x_, _ ,_ = x
+#
+#         B, C, H, W = x_.shape
+#
+#         x = self.fpn([*x])
+#         x_raw = self.head(x)
+#
+#         batched_img_metas = [self.cfg['metas']] * B
+#         x = self.head.predict_by_feat(*x_raw, batch_img_metas=batched_img_metas, cfg=self.cfg['nms'], with_nms=True)
+#
+#         return x_raw, x
+#
+#     def get_loss(self, x_raw, y: [InstanceData]):
+#         batched_img_metas = [self.cfg['metas']] * len(y)
+#
+#         loss_dict = self.head.loss_by_feat(*x_raw, batch_img_metas=batched_img_metas, batch_gt_instances=y)
+#
+#         return loss_dict
+#
+# class RepViTDet(nn.Module):
+#     def __init__(self, arch='m1', n_classes=1, img_size=1024, fuse=True, freeze=True):
+#         super().__init__()
+#         # upgrade to handle archs
+#
+#         self.backbone = RepViT(arch, img_size, fuse, freeze, True,
+#                                out_indices=['stage1', 'stage2', 'stage3', 'final'])
+#         self.rpn = RpnYolox(input_shape=img_size)
+#
+#     def forward(self, x):
+#
+#         x_enc = self.backbone(x)
+#
+#         x1, x2, x3, xf = x_enc
+#
+#         x_raw, x_pred = self.rpn([x1, x2, x3])
+#
+#         return xf, x_raw, x_pred
+#
+#     def get_loss(self, x_raw, y):
+#         return self.rpn.get_loss(x_raw, y)
+#
